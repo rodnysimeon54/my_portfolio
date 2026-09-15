41a608897253fc490063bd7262b90ced097162b7
Rodny Siméon <rodnysimeon54@gmail.com>
2026-09-15 15:28:10 -0400

    Make email settings optional

--- config/settings.py
+++ config/settings.py
@@ -108,29 +108,33 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
 # =========================
 # EMAIL GMAIL
 # =========================
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 
-EMAIL_PORT = 587
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = config(
+    'DEFAULT_FROM_EMAIL',
+    default='webmaster@localhost'
+)
 
-CONTACT_EMAIL = config('CONTACT_EMAIL')
+CONTACT_EMAIL = config(
+    'CONTACT_EMAIL',
+    default=''
+)
 
 EMAIL_TIMEOUT = 30
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ==============================
